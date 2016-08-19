3f0c195672fc96e5b7d6771e491b4984dcd88a25
Quentin Nerden <quentin.nerden@gmail.com>
2016-08-19 11:26:17 +0200

    Fix program to pass last test.

--- exercises/bob/example.sh
+++ exercises/bob/example.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
 
-input=$1
-input="${input#"${input%%[![:space:]]*}"}"   # remove leading whitespace characters
-input="${input%"${input##*[![:space:]]}"}"   # remove trailing whitespace characters
+# Get what Bob says.
+# %b interprets escapted characters (like '\n' for newline)
+input="$(printf %b "${1}")"
+
+# Remove space characters
+input="${input//[[:space:]]/}"
 
 # Is there silence?
-if [ "$input" == "" ]; then
+if [[ "${input}" == "" ]]; then
   echo "Fine. Be that way!"
   exit 0
 fi
 
-# clean string up(to make later parsing easier)
-input=${input// /}  # remove inside space characters
-input=${input//[1-9]/}  # remove numbers
-input=${input//[\%\^\*\&\$\#\@\\\~]/}  # remove special characters
-input=${input//[\[\]\(\)\{\}\⟨\⟩\:\'\"\`\-\_\.\,\;\!]/}  # remove punctuation signs (leave '?' it indicates a question)
-
-# Is there shouting?
-if [ ${input%%*[^[:upper:]\?]*} ]; then
-  if [ ${input%%*[^[:upper:]\?]*} != "?" ]; then  # in case there is just number and '?'
-    echo "Whoa, chill out!"
-    exit 0
-  fi
+# Is there shouting (capital letter and no lowercase letters)
+if [[ "$input" == *[[:upper:]]* ]] && [[ "$input" != *[[:lower:]]* ]]; then
+  echo "Whoa, chill out!"
+  exit 0
 fi
 
-# Is it a question?
-if [ "${input: -1}" == "?" ]; then
+# Is it a question? (last character is a '?')
+if [[ "${input: -1}" == "?" ]]; then
   echo "Sure."
   exit 0
 fi
