37c25ebe352303e2fdf81a2191e1c5f036fba282
Quentin Nerden <quentin.nerden@gmail.com>
2016-09-19 22:52:42 +0200

    Add bash3boilerplate bits. DRY up code
    (avoid repeating echo "..." and exit 0

--- exercises/bob/example.sh
+++ exercises/bob/example.sh
@@ -1,29 +1,53 @@
 #!/bin/bash
 
+# Exit on error. Append || true if you expect an error.
+set -o errexit
+# Exit on error inside any functions or subshells.
+set -o errtrace
+# Do not allow use of undefined vars. Use ${VAR:-} to use an undefined VAR
+set -o nounset
+# Catch error even if it's not in after the last pipe.
+set -o pipefail
+# Turn on traces, useful while debugging but commented out by default
+# set -o xtrace
+
+# Set magic variables for current file, directory, os, etc.
+#__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+#__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+#__base="$(basename ${__file} .sh)"
+
+# print argument and exit
+function say() {
+  local mesg=${1}
+  echo ${mesg}
+  exit 0
+}
+
+if [[ $# -eq 0 ]]; then
+  say "Fine. Be that way!"
+fi
+
 # Get what Bob says.
 # %b interprets escapted characters (like '\n' for newline)
 input="$(printf %b "${1}")"
 
 # Remove space characters
 input="${input//[[:space:]]/}"
-input="${input//$(printf '\u00a0')}"  # [[:space:]] doesn't match \u00a0 on Linux
+input="${input//$(printf '\u00a0')}" # [[:space:]] doesn't match \u00a0 on Linux
 
 # Is there silence?
 if [[ "${input}" == "" ]]; then
-  echo "Fine. Be that way!"
-  exit 0
+  say "Fine. Be that way!"
 fi
 
 # Is there shouting (capital letter and no lowercase letters)
 if [[ "$input" == *[[:upper:]]* ]] && [[ "$input" != *[[:lower:]]* ]]; then
-  echo "Whoa, chill out!"
-  exit 0
+  say "Whoa, chill out!"
 fi
 
 # Is it a question? (last character is a '?')
 if [[ "${input: -1}" == "?" ]]; then
-  echo "Sure."
-  exit 0
+  say "Sure."
 fi
 
 # Is it a regular statement ?
