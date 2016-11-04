04ac425bd8dbb07b20c5fcb57812da8a8e6366fb
Brandon Keepers <bkeepers@github.com>
2016-11-04 12:15:47 -0500

    Remove intermediate exercises from hello-world

--- exercises/hello-world/hello_world_test.sh
+++ exercises/hello-world/hello_world_test.sh
@@ -18,17 +18,3 @@
   [ "$status" -eq 0 ]
   [ "$output" = "Hello, Bob!" ]
 }
-
-@test 'When given an empty string it should have a space and punctuation, though admittedly this is strange.' {
-  run bash hello_world.sh ""
-
-  [ "$status" -eq 0 ]
-  [ "$output" = "Hello, !" ]
-}
-
-@test 'When given "Alice and Bob" it greets them both' {
-  run bash hello_world.sh Alice and Bob
-
-  [ "$status" -eq 0 ]
-  [ "$output" = "Hello, Alice and Bob!" ]
-}
