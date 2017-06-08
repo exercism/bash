c4b5a1ee623d51f8cb9a054a37de6ad40146e14c
Ryan Palo <ryan@thepalos.com>
2017-06-07 21:31:31 -0700

    Make bonus tests with flags

--- exercises/difference-of-squares/difference_of_squares_test.sh
+++ exercises/difference-of-squares/difference_of_squares_test.sh
@@ -21,3 +21,51 @@
     [ "$output" == 25164150 ]
 }
 
+# Bonus!  Add flags for individual functions
+@test 'square of sum 1' {
+    skip
+    run bash difference_of_squares.sh 1 -S
+
+    [ "$status" -eq 0 ]
+    [ "$output" == 1 ]
+}
+
+@test 'square of sum 5' {
+    skip
+    run bash difference_of_squares.sh 5 -S
+
+    [ "$status" -eq 0 ]
+    [ "$output" == 225 ]
+}
+
+@test 'square of sum 100' {
+    skip
+    run bash difference_of_squares.sh 100 -S
+
+    [ "$status" -eq 0 ]
+    [ "$output" == 25502500 ]
+}
+
+@test 'sum of squares 1' {
+    skip
+    run bash difference_of_squares.sh 1 -s
+
+    [ "$status" -eq 0 ]
+    [ "$output" == 1 ]
+}
+
+@test 'sum of squares 5' {
+    skip
+    run bash difference_of_squares.sh 5 -s
+
+    [ "$status" -eq 0 ]
+    [ "$output" == 55 ]
+}
+
+@test 'sum of squares 100' {
+    skip
+    run bash difference_of_squares.sh 100 -s
+
+    [ "$status" -eq 0 ]
+    [ "$output" == 338350 ]
+}
