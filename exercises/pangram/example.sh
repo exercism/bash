1cc8935aa9216168515eeb6870965d3224255659
James Robson <jaggededgedjustice@gmail.com>
2017-06-18 15:46:43 +0100

    pangram exercise

---
+++ exercises/pangram/example.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+input=$1
+#delete non-letters then lowercase, count number of unique letters
+letter_count=$(echo $input | tr -dc [:alpha:] | tr '[:upper:]' '[:lower:]' | fold -w1 | sort -u | wc -l)
+
+test $letter_count -eq 26
