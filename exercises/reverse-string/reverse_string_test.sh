6d906a34103962b1c3f4e6dc947175321434431b
Sam Warner <sam.warner@blackpepper.co.uk>
2017-12-19 21:02:39 +0000

    reverse-string: add to track

---
+++ exercises/reverse-string/reverse_string_test.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bats
+
+@test "An empty string" {
+  #skip
+  run bash reverse_string.sh
+
+  [ "$status" -eq 0 ]
+  [ "$output" = "" ]
+}
+
+@test "A word" {
+  skip
+  run bash reverse_string.sh robot
+
+  [ "$status" -eq 0 ]
+  [ "$output" = "tobor" ]
+}
+
+@test "A capitalised word" { 
+  skip
+  run bash reverse_string.sh Ramen
+
+  [ "$status" -eq 0 ]
+  [ "$output" = "nemaR" ]
+}
+
+@test "A sentence with punctuation" { 
+  skip
+  run bash reverse_string.sh "I'm hungry!"
+
+  [ "$status" -eq 0 ]
+  [ "$output" = "!yrgnuh m'I" ]
+}
+
+@test "A palindrome" { 
+  skip
+  run bash reverse_string.sh racecar
+
+  [ "$status" -eq 0 ]
+  [ "$output" = "racecar" ]
+}
