c9973f52836af89553aa9c7d5401154f176c752e
Mugur (Bud) Chirica <chirica.mugurel@gmail.com>
2018-01-04 15:21:50 +0000

    Update test suite to check for output instead of exit code

--- exercises/armstrong-numbers/armstrong_numbers_test.sh
+++ exercises/armstrong-numbers/armstrong_numbers_test.sh
@@ -1,58 +1,66 @@
 #!/usr/bin/env bats
 
-@test 'Single digits are armstrong numbers' {
-  #skip
+@test 'Single digits are Armstrong numbers' {
+  # skip
   run ./armstrong_numbers.sh isarmstrong 5
 
   [ "$status" -eq 0 ]
+  [ "$output" = "true" ]
 }
 
-@test 'There are no two digit armstrong numbers' {
-  skip
+@test 'There are no two digit Armstrong numbers' {
+  # skip
   run ./armstrong_numbers.sh isarmstrong 10
 
   [ "$status" -eq 1 ]
+  [ "$output" = "false" ]
 }
 
-@test 'A three digit number that is an armstrong number' {
-  skip
+@test 'A three digit number that is an Armstrong number' {
+  # skip
   run ./armstrong_numbers.sh isarmstrong 153
 
   [ "$status" -eq 0 ]
+  [ "$output" = "true" ]
 }
 
-@test 'A three digit number that is not an armstrong number' {
-  skip
-  run ./armstrong_numbers.sh isarmstrong 100 
+@test 'A three digit number that is not an Armstrong number' {
+  # skip
+  run ./armstrong_numbers.sh isarmstrong 100
 
   [ "$status" -eq 1 ]
+  [ "$output" = "false" ]
 }
 
-@test 'A four digit number that is an armstrong number' {
-  skip
-  run ./armstrong_numbers.sh isarmstrong 9475 
+@test 'A four digit number that is an Armstrong number' {
+  # skip
+  run ./armstrong_numbers.sh isarmstrong 9474
 
   [ "$status" -eq 0 ]
+  [ "$output" = "true" ]
 }
 
-@test 'A four digit number that is not an armstrong number' {
-  skip
-  run ./armstrong_numbers.sh isarmstrong 9475 
+@test 'A four digit number that is not an Armstrong number' {
+  # skip
+  run ./armstrong_numbers.sh isarmstrong 9475
 
   [ "$status" -eq 1 ]
+  [ "$output" = "false" ]
 }
 
-@test 'A seven digit number that is an armstrong number' {
-  skip
-  run ./armstrong_numbers.sh isarmstrong 9926315 
+@test 'A seven digit number that is an Armstrong number' {
+  # skip
+  run ./armstrong_numbers.sh isarmstrong 9926315
 
   [ "$status" -eq 0 ]
+  [ "$output" = "true" ]
 }
 
-@test 'A seven digit number that is not an armstrong number' {
-  skip
-  run ./armstrong_numbers.sh isarmstrong 9926314 
+@test 'A seven digit number that is not an Armstrong number' {
+  # skip
+  run ./armstrong_numbers.sh isarmstrong 9926314
 
   [ "$status" -eq 1 ]
+  [ "$output" = "false" ]
 }
 
