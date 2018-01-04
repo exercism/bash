26287eb8f4420e3598764ce456099bfd529e24ad
budmc29 <chirica.mugurel@gmail.com>
2018-01-04 23:07:42 +0000

    Fix example implementation

--- exercises/armstrong-numbers/armstrong_numbers_test.sh
+++ exercises/armstrong-numbers/armstrong_numbers_test.sh
@@ -2,63 +2,63 @@
 
 @test 'Single digits are Armstrong numbers' {
   # skip
-  run ./armstrong_numbers.sh isarmstrong 5
+  run ./armstrong_numbers.sh is_armstrong 5
 
   [ "$status" -eq 0 ]
   [ "$output" = "true" ]
 }
 
 @test 'There are no two digit Armstrong numbers' {
-  # skip
-  run ./armstrong_numbers.sh isarmstrong 10
+  skip
+  run ./armstrong_numbers.sh is_armstrong 10
 
   [ "$status" -eq 1 ]
   [ "$output" = "false" ]
 }
 
 @test 'A three digit number that is an Armstrong number' {
-  # skip
-  run ./armstrong_numbers.sh isarmstrong 153
+  skip
+  run ./armstrong_numbers.sh is_armstrong 153
 
   [ "$status" -eq 0 ]
   [ "$output" = "true" ]
 }
 
 @test 'A three digit number that is not an Armstrong number' {
-  # skip
-  run ./armstrong_numbers.sh isarmstrong 100
+  skip
+  run ./armstrong_numbers.sh is_armstrong 100
 
   [ "$status" -eq 1 ]
   [ "$output" = "false" ]
 }
 
 @test 'A four digit number that is an Armstrong number' {
-  # skip
-  run ./armstrong_numbers.sh isarmstrong 9474
+  skip
+  run ./armstrong_numbers.sh is_armstrong 9474
 
   [ "$status" -eq 0 ]
   [ "$output" = "true" ]
 }
 
 @test 'A four digit number that is not an Armstrong number' {
-  # skip
-  run ./armstrong_numbers.sh isarmstrong 9475
+  skip
+  run ./armstrong_numbers.sh is_armstrong 9475
 
   [ "$status" -eq 1 ]
   [ "$output" = "false" ]
 }
 
 @test 'A seven digit number that is an Armstrong number' {
-  # skip
-  run ./armstrong_numbers.sh isarmstrong 9926315
+  skip
+  run ./armstrong_numbers.sh is_armstrong 9926315
 
   [ "$status" -eq 0 ]
   [ "$output" = "true" ]
 }
 
 @test 'A seven digit number that is not an Armstrong number' {
-  # skip
-  run ./armstrong_numbers.sh isarmstrong 9926314
+  skip
+  run ./armstrong_numbers.sh is_armstrong 9926314
 
   [ "$status" -eq 1 ]
   [ "$output" = "false" ]
