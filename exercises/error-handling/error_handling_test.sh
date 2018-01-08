094b578e2153898b9bc2da6c61d2e5f3655b2cb8
Mugur (Bud) Chirica <chirica.mugurel@gmail.com>
2018-01-08 16:37:00 +0000

    Update error handling to be CI friendly and not enforce opinions

--- exercises/error-handling/error_handling_test.sh
+++ exercises/error-handling/error_handling_test.sh
@@ -1,36 +1,32 @@
 #!/usr/bin/env bats
 
 @test "correct arguments" {
-    #skip
-    run bash error_handling.sh Alice
-    
-    [ "$status" -eq 0 ]
-    [ "$output" = "Hello, Alice" ]
+  #skip
+  run bash error_handling.sh Alice
+
+  [ "$status" -eq 0 ]
+  [ "$output" = "Hello, Alice" ]
 }
 
 @test "one long argument" {
-    skip
-    run bash error_handling.sh "Alice and Bob"
-    
-    [ "$status" -eq 0 ]
-    [ "$output" = "Hello, Alice and Bob" ]
+  skip
+  run bash error_handling.sh "Alice and Bob"
+
+  [ "$status" -eq 0 ]
+  [ "$output" = "Hello, Alice and Bob" ]
 }
 
 @test "incorrect arguments" {
-    skip
-    run bash error_handling.sh Alice Bob
+  skip
+  run bash error_handling.sh Alice Bob
 
-    [ "$status" -ne 0 ]
+  [ "$status" -ne 0 ]
 }
 
-@test "use errexit" {
-    skip
-    source error_handling.sh
-    [ echo $SHELLOPTS | grep -q 'errexit' ]
-}
+@test "print usage banner with no value given" {
+  skip
+  run bash error_handling.sh
 
-@test "use nounset" {
-    skip
-    source error_handling.sh
-    [ echo $SHELLOPTS | grep -q 'nounset' ]
+  [ "$status" -eq 1 ]
+  [ "$output" = "Usage: ./error_handling <greetee>" ]
 }
