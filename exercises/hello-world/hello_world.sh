5df2382253bd877b30f7f8231a3cea2e2fabb5a9
Mugur (Bud) Chirica <chirica.mugurel@gmail.com>
2018-01-29 16:08:10 +0000

    Add starting template

---
+++ exercises/hello-world/hello_world.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# This is a bash script template in order to help you quick start any script.
+# It contains some sensible defaults, you can learn more by visiting:
+# https://google.github.io/styleguide/shell.xml
+
+# This option will make the script exit when there is an error
+set -o errexit
+# This option will make the script exit when are unset variables
+set -o nounset
+
+main() {
+  # A string variable containing only the FIRST argument passed to the script,
+  # you can use input=$@ to get a string with ALL arguments
+  input=$1
+
+  # Add your code here
+}
+
+# Calls the main function passing all the arguments to it via '$@'
+# The argument is in quotes to prevent whitespace issues
+main "$@"
---
+++ exercises/reverse-string/reverse_string.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+main() {
+  input=$1
+
+  # Add your code here
+}
+
+main "$@"
