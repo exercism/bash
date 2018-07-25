55813190cf57c63e6d4356dab9bc49e2062b2660
Guy Gastineau <strings.stringsandstrings@gmail.com>
2018-07-24 20:20:02 -0400

    updated the test for diff-of-sqr; added version file

--- exercises/difference-of-squares/difference_of_squares_test.sh
+++ exercises/difference-of-squares/difference_of_squares_test.sh
@@ -1,74 +1,70 @@
 #!/usr/bin/env bats
 
-@test 'difference of squares 1' {
-    #skip
-    run bash difference_of_squares.sh 1
+# Version = 1.2.0
 
-    [ "$status" -eq 0 ]
-    [ "$output" == 0 ]
+# Test to Square the sum of the numbers up to the given number
+@test "square of sum 1" {
+  #skip
+  run bash difference_of_squares.sh 1 -S
+  [ "$status" -eq 0 ]
+  [ "$output" = "1" ]
 }
 
-@test 'difference of squares 5' {
-    skip
-    run bash difference_of_squares.sh 5
-
-    [ "$status" -eq 0 ]
-    [ "$output" == 170 ]
+@test "square of sum 5" {
+  skip
+  run bash difference_of_squares.sh 5 -S
+  [ "$status" -eq 0 ]
+  [ "$output" = "225" ]
 }
 
-@test 'difference of squares 100' {
-    skip
-    run bash difference_of_squares.sh 100
-
-    [ "$status" -eq 0 ]
-    [ "$output" == 25164150 ]
+@test "square of sum 100" {
+  skip
+  run bash difference_of_squares.sh 100 -S
+  [ "$status" -eq 0 ]
+  [ "$output" = "25502500" ]
 }
 
-# Bonus!  Add flags for individual functions
-@test 'square of sum 1' {
-    skip
-    run bash difference_of_squares.sh 1 -S
-
-    [ "$status" -eq 0 ]
-    [ "$output" == 1 ]
+# Test to Sum the squares of the numbers up to the given number
+@test "sum of squares 1" {
+  skip
+  run bash difference_of_squares.sh 1 -s
+  [ "$status" -eq 0 ]
+  [ "$output" = "1" ]
 }
 
-@test 'square of sum 5' {
-    skip
-    run bash difference_of_squares.sh 5 -S
-
-    [ "$status" -eq 0 ]
-    [ "$output" == 225 ]
+@test "sum of squares 5" {
+  skip
+  run bash difference_of_squares.sh 5 -s
+  [ "$status" -eq 0 ]
+  [ "$output" = "55" ]
 }
 
-@test 'square of sum 100' {
-    skip
-    run bash difference_of_squares.sh 100 -S
-
-    [ "$status" -eq 0 ]
-    [ "$output" == 25502500 ]
+@test "sum of squares 100" {
+  skip
+  run bash difference_of_squares.sh 100 -s
+  [ "$status" -eq 0 ]
+  [ "$output" = "338350" ]
 }
 
-@test 'sum of squares 1' {
-    skip
-    run bash difference_of_squares.sh 1 -s
-
-    [ "$status" -eq 0 ]
-    [ "$output" == 1 ]
+# Test to Subtract sum of squares from square of sums
+@test "difference of squares 1" {
+  skip
+  run bash difference_of_squares.sh 1
+  [ "$status" -eq 0 ]
+  [ "$output" = "0" ]
 }
 
-@test 'sum of squares 5' {
-    skip
-    run bash difference_of_squares.sh 5 -s
-
-    [ "$status" -eq 0 ]
-    [ "$output" == 55 ]
+@test "difference of squares 5" {
+  skip
+  run bash difference_of_squares.sh 5
+  [ "$status" -eq 0 ]
+  [ "$output" = "170" ]
 }
 
-@test 'sum of squares 100' {
-    skip
-    run bash difference_of_squares.sh 100 -s
-
-    [ "$status" -eq 0 ]
-    [ "$output" == 338350 ]
+@test "difference of squares 100" {
+  skip
+  run bash difference_of_squares.sh 100
+  [ "$status" -eq 0 ]
+  [ "$output" = "25164150" ]
 }
+
