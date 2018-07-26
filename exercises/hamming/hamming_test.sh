063f02171dea25cb05280d83db0e5e20c4dc6d08
Guy Gastineau <strings.stringsandstrings@gmail.com>
2018-07-26 15:23:31 -0400

    fixed the output line of the hamming test file

--- exercises/hamming/hamming_test.sh
+++ exercises/hamming/hamming_test.sh
@@ -4,91 +4,91 @@
   #skip
   run bash hamming.sh "" ""
   [ "$status" -eq 0 ]
-  [ "$output" == "0" ]
+  [ "$output" -eq 0 ]
 }
 
 @test "identical strands" {
   skip
   run bash hamming.sh "A" "A"
   [ "$status" -eq 0 ]
-  [ "$output" == "0" ]
+  [ "$output" -eq 0 ]
 }
 
 @test "long identical strands" {
   skip
   run bash hamming.sh "GGACTGA" "GGACTGA"
   [ "$status" -eq 0 ]
-  [ "$output" == "0" ]
+  [ "$output" -eq 0 ]
 }
 
 @test "complete distance in single nucleotide strands" {
   skip
   run bash hamming.sh "A" "G"
   [ "$status" -eq 0 ]
-  [ "$output" == "1" ]
+  [ "$output" -eq 1 ]
 }
 
 @test "complete distance in small strands" {
   skip
   run bash hamming.sh "AG" "CT"
   [ "$status" -eq 0 ]
-  [ "$output" == "2" ]
+  [ "$output" -eq 2 ]
 }
 
 @test "small distance in small strands" {
   skip
   run bash hamming.sh "AT" "CT"
   [ "$status" -eq 0 ]
-  [ "$output" == "1" ]
+  [ "$output" -eq 1 ]
 }
 
 @test "small distance" {
   skip
   run bash hamming.sh "GGACG" "GGTCG"
   [ "$status" -eq 0 ]
-  [ "$output" == "1" ]
+  [ "$output" -eq 1 ]
 }
 
 @test "small distance in long strands" {
   skip
   run bash hamming.sh "ACCAGGG" "ACTATGG"
   [ "$status" -eq 0 ]
-  [ "$output" == "2" ]
+  [ "$output" -eq 2 ]
 }
 
 @test "non-unique character in first strand" {
   skip
   run bash hamming.sh "AAG" "AAA"
   [ "$status" -eq 0 ]
-  [ "$output" == "1" ]
+  [ "$output" -eq 1 ]
 }
 
 @test "non-unique character in second strand" {
   skip
   run bash hamming.sh "AAA" "AAG"
   [ "$status" -eq 0 ]
-  [ "$output" == "1" ]
+  [ "$output" -eq 1 ]
 }
 
 @test "same nucleotides in different positions" {
   skip
   run bash hamming.sh "TAG" "GAT"
   [ "$status" -eq 0 ]
-  [ "$output" == "2" ]
+  [ "$output" -eq 2 ]
 }
 
 @test "large distance" {
   skip
   run bash hamming.sh "GATACA" "GCATAA"
   [ "$status" -eq 0 ]
-  [ "$output" == "4" ]
+  [ "$output" -eq 4 ]
 }
 
 @test "large distance in off-by-one strand" {
   skip
   run bash hamming.sh "GGACGGATTCTG" "AGGACGGATTCT"
   [ "$status" -eq 0 ]
-  [ "$output" == "9" ]
+  [ "$output" -eq 9 ]
 }
 
 @test "disallow first strand longer" {
