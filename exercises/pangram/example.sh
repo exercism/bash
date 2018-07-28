6fc1481fdf0b23804348c21a4cabd65a5b6a8ca1
Guy Gastineau <strings.stringsandstrings@gmail.com>
2018-07-28 18:33:54 -0400

    Updated pangram; added version file "1.4.1" (#193)
    * Updated pangram; added version file "1.4.0"
    
    * refined pangram test form; modified example.sh to pass; updated version to 1.4.1

--- exercises/pangram/example.sh
+++ exercises/pangram/example.sh
@@ -4,4 +4,6 @@ input=$1
 #delete non-letters then lowercase, count number of unique letters
 letter_count=$(echo $input | tr -dc [:alpha:] | tr '[:upper:]' '[:lower:]' | fold -w1 | sort -u | wc -l)
 
-test $letter_count -eq 26
+[[ $letter_count -eq 26 ]] && echo "true" && exit 0
+
+echo "false" && exit 0
