e6c5ee7407eafae7e651b471a29e70daf6b0cf07
Guy Gastineau <strings.stringsandstrings@gmail.com>
2018-08-01 06:39:41 -0400

    updated grains test; modified example.sh to pass new test; added vers… (#199)
    * updated grains test; modified example.sh to pass new test; added version file
    
    * fixed capitalization in test
    
    * made changes per request

--- exercises/grains/grains_test.sh
+++ exercises/grains/grains_test.sh
@@ -1,81 +1,79 @@
 #!/usr/bin/env bats
 
-@test "Square 1" {
+@test "1" {
   #skip
   run bash grains.sh 1
-
   [ "$status" -eq 0 ]
-  [ "$output" = "1" ]
+  [ "$output" == "1" ]
 }
 
-@test "Square 2" {
-  #skip
+@test "2" {
+  skip
   run bash grains.sh 2
-
   [ "$status" -eq 0 ]
-  [ "$output" = "2" ]
+  [ "$output" == "2" ]
 }
 
-@test "Square 3" {
-  #skip
+@test "3" {
+  skip
   run bash grains.sh 3
-
   [ "$status" -eq 0 ]
-  [ "$output" = "4" ]
+  [ "$output" == "4" ]
 }
 
-@test "Square 4" {
-  #skip
+@test "4" {
+  skip
   run bash grains.sh 4
-
   [ "$status" -eq 0 ]
-  [ "$output" = "8" ]
+  [ "$output" == "8" ]
 }
 
-@test "Square 16" {
-  #skip
+@test "16" {
+  skip
   run bash grains.sh 16
-
   [ "$status" -eq 0 ]
-  [ "$output" = "32768" ]
+  [ "$output" == "32768" ]
 }
 
-@test "Square 32" {
-  #skip
+@test "32" {
+  skip
   run bash grains.sh 32
-
   [ "$status" -eq 0 ]
-  [ "$output" = "2147483648" ]
+  [ "$output" == "2147483648" ]
 }
 
-@test "Square 64" {
-  #skip
+@test "64" {
+  skip
   run bash grains.sh 64
-
   [ "$status" -eq 0 ]
-  [ "$output" = "9223372036854775808" ]
+  [ "$output" == "9223372036854775808" ]
 }
 
-@test "Square 0 causes error" {
-  #skip
+@test "square 0 raises an exception" {
+  skip
   run bash grains.sh 0
-
   [ "$status" -eq 1 ]
-  [ "$output" = "Error: invalid input" ]
+  [ "$output" == "Error: invalid input" ]
 }
 
-@test "Square -1 causes error" {
-  #skip
+@test "negative square raises an exception" {
+  skip
   run bash grains.sh -1
-
   [ "$status" -eq 1 ]
-  [ "$output" = "Error: invalid input" ]
+  [ "$output" == "Error: invalid input" ]
 }
 
-@test "Square 65 causes error" {
-  #skip
+@test "square greater than 64 raises an exception" {
+  skip
   run bash grains.sh 65
-
   [ "$status" -eq 1 ]
-  [ "$output" = "Error: invalid input" ]
+  [ "$output" == "Error: invalid input" ]
 }
+
+@test "returns the total number of grains on the board" {
+  skip
+  run bash grains.sh total
+  [ "$status" -eq 0 ]
+  [ "$output" == "18446744073709551615" ]
+}
+
