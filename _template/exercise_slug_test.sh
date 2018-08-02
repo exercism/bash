ab6e84fe92aee702993f6770faaa9ace34ad4c3e
Guy Gastineau <strings.stringsandstrings@gmail.com>
2018-08-02 18:35:18 -0400

    added link to and explanation of the test generator to the contributi… (#209)
    * added link to and explanation of the test generator to the contributing doc
    
    * made the description more persuasive

--- _template/exercise_slug_test.sh
+++ _template/exercise_slug_test.sh
@@ -3,7 +3,6 @@
 @test "Test name - first test shouldn't be skipped" {
   #skip
   run bash exercise_slug.sh
-
   [ "$status" -eq 0 ]
   [ "$output" = "What's expected" ]
 }
@@ -11,7 +10,6 @@
 @test "Second test onwards should be skipped" {
   skip
   run bash exercise_slug.sh
-
   [ "$status" -eq 0 ]
   [ "$output" = "What's expected" ]
 }
