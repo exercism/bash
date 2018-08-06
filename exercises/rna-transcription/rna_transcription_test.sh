c338c1d88a107d7a8b9ce3616118dd62c675e366
Guy Gastineau <strings.stringsandstrings@gmail.com>
2018-08-06 00:20:17 -0400

    updated rna_transcription_test.sh; added .meta/version file

--- exercises/rna-transcription/rna_transcription_test.sh
+++ exercises/rna-transcription/rna_transcription_test.sh
@@ -1,65 +1,65 @@
 #!/usr/bin/env bats
 
-@test 'Complement of cytosine is guanine' {
+@test "Empty RNA sequence" {
   #skip
-  run bash rna_transcription.sh C
-
+  run bash rna_transcription.sh
   [ "$status" -eq 0 ]
-  [ "$output" == 'G' ]
+  [ "$output" == "" ]
 }
 
-@test 'Complement of guanine is cytosine' {
+@test "RNA complement of cytosine is guanine" {
+  skip
+  run bash rna_transcription.sh C
+  [ "$status" -eq 0 ]
+  [ "$output" == "G" ]
+}
+
+@test "RNA complement of guanine is cytosine" {
   skip
   run bash rna_transcription.sh G
-  
   [ "$status" -eq 0 ]
-  [ "$output" == 'C' ]
+  [ "$output" == "C" ]
 }
 
-@test 'Complement of thymine is adenine' {
+@test "RNA complement of thymine is adenine" {
   skip
   run bash rna_transcription.sh T
-
   [ "$status" -eq 0 ]
-  [ "$output" == 'A' ]
+  [ "$output" == "A" ]
 }
 
-@test 'Complement of adenine is uracil' {
+@test "RNA complement of adenine is uracil" {
   skip
   run bash rna_transcription.sh A
-
   [ "$status" -eq 0 ]
-  [ "$output" == 'U' ]
+  [ "$output" == "U" ]
 }
 
-@test 'Complement of long string' {
+@test "RNA complement" {
   skip
   run bash rna_transcription.sh ACGTGGTCTTAA
-
   [ "$status" -eq 0 ]
-  [ "$output" == 'UGCACCAGAAUU' ]
+  [ "$output" == "UGCACCAGAAUU" ]
 }
 
-@test 'Handles invalid character' {
+@test "Handles invalid character" {
   skip
-  run bash rna_transcription.sh U
-
+  run bash rna_transcription.sh ACGTXCTTA
   [ "$status" -eq 1 ]
-  [ "$output" == 'Invalid nucleotide detected.' ]
+  [ "$output" == "Invalid nucleotide detected." ]
 }
 
-@test 'Handles completely invalid string' {
+@test "Handles completely invalid string" {
   skip
   run bash rna_transcription.sh XXXX
-
   [ "$status" -eq 1 ]
-  [ "$output" == 'Invalid nucleotide detected.' ]
+  [ "$output" == "Invalid nucleotide detected." ]
 }
 
-@test 'Handles partially invalid string' {
+@test "Handles partially invalid string" {
   skip
   run bash rna_transcription.sh ACGTXCTTAA
-
   [ "$status" -eq 1 ]
-  [ "$output" == 'Invalid nucleotide detected.' ]
+  [ "$output" == "Invalid nucleotide detected." ]
 }
+
