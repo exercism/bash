b6de0ef623c5fb3f3a2c28599b50316a7508f03e
Guy Gastineau <strings.stringsandstrings@gmail.com>
2018-08-15 19:30:11 -0400

    isogram: added new exercise

---
+++ exercises/isogram/example.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+LOWERCASE_INPUT="${1,,}"
+
+isogram_candidate="${LOWERCASE_INPUT//[![:lower:]]}"
+
+for letter in {a..z}; do # remove aech letter only once
+  isogram_candidate="${isogram_candidate/$letter/}"
+done
+
+[[ -z $isogram_candidate ]] && echo 'true' || echo 'false'
