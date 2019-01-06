a78407424e7a09bf42c9657b1eff787f58759997
guy <strings.stringandstrings@gmail.com>
2019-01-06 13:04:28 -0500

    fixed issues in bash hamming

--- exercises/hamming/hamming_test.sh
+++ exercises/hamming/hamming_test.sh
@@ -1,57 +1,57 @@
-#!/usr/bin/env bats
+#!/usr/bin/env bash
 
-@test "empty strands" {
+@test 'empty strands' {
   #skip
-  run bash hamming.sh "" ""
+  run bash hamming.sh '' ''
   [ "$status" -eq 0 ]
   [ "$output" -eq 0 ]
 }
 
-@test "single letter identical strands" {
+@test 'single letter identical strands' {
   skip
-  run bash hamming.sh "A" "A"
+  run bash hamming.sh 'A' 'A'
   [ "$status" -eq 0 ]
   [ "$output" -eq 0 ]
 }
 
-@test "single letter different strands" {
+@test 'single letter different strands' {
   skip
-  run bash hamming.sh "G" "T"
+  run bash hamming.sh 'G' 'T'
   [ "$status" -eq 0 ]
   [ "$output" -eq 1 ]
 }
 
-@test "long identical strands" {
+@test 'long identical strands' {
   skip
-  run bash hamming.sh "GGACTGAAATCTG" "GGACTGAAATCTG"
+  run bash hamming.sh 'GGACTGAAATCTG' 'GGACTGAAATCTG'
   [ "$status" -eq 0 ]
   [ "$output" -eq 0 ]
 }
 
-@test "long different strands" {
+@test 'long different strands' {
   skip
-  run bash hamming.sh "GGACGGATTCTG" "AGGACGGATTCT"
+  run bash hamming.sh 'GGACGGATTCTG' 'AGGACGGATTCT'
   [ "$status" -eq 0 ]
   [ "$output" -eq 9 ]
 }
 
-@test "disallow first strand longer" {
+@test 'disallow first strand longer' {
   skip
-  run bash hamming.sh "AATG" "AAA"
+  run bash hamming.sh 'AATG' 'AAA'
   [ "$status" -eq 1 ]
-  [ "$output" == "left and right strands must be of equal length" ]
+  [ "$output" == 'left and right strands must be of equal length' ]
 }
 
-@test "disallow second strand longer" {
+@test 'disallow second strand longer' {
   skip
-  run bash hamming.sh "ATA" "AGTG"
+  run bash hamming.sh 'ATA' 'AGTG'
   [ "$status" -eq 1 ]
-  [ "$output" == "left and right strands must be of equal length" ]
+  [ "$output" == 'left and right strands must be of equal length' ]
 }
 
 @test "no input" {
   skip
   run bash hamming.sh
   [ "$status" -eq 1 ]
-  [ "$output" == "Usage: hamming.sh <string1> <string2>" ]
+  [ "$output" == 'Usage: hamming.sh <string1> <string2>' ]
 }
