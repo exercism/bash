b6995e392f6fc7c5b418be30b4a15bd04ed06250
Glenn Jackman <jackman@pythian.com>
2019-05-17 12:19:49 -0400

    make test names unique

--- exercises/beer-song/beer_song_test.sh
+++ exercises/beer-song/beer_song_test.sh
@@ -9,7 +9,7 @@ Take one down and pass it around, 98 bottles of beer on the wall."
     [[ $output == "$expected" ]]
 }
 
-@test 'last_generic_verse' {
+@test '3rd_last_generic_verse' {
     skip
     expected="3 bottles of beer on the wall, 3 bottles of beer.
 Take one down and pass it around, 2 bottles of beer on the wall."
@@ -18,7 +18,7 @@ Take one down and pass it around, 2 bottles of beer on the wall."
     [[ $output == "$expected" ]]
 }
 
-@test 'last_generic_verse' {
+@test '2nd_last_generic_verse' {
     skip
     expected="2 bottles of beer on the wall, 2 bottles of beer.
 Take one down and pass it around, 1 bottle of beer on the wall."
@@ -27,7 +27,7 @@ Take one down and pass it around, 1 bottle of beer on the wall."
     [[ $output == "$expected" ]]
 }
 
-@test 'last_generic_verse' {
+@test 'penultimate_verse' {
     skip
     expected="1 bottle of beer on the wall, 1 bottle of beer.
 Take it down and pass it around, no more bottles of beer on the wall."
