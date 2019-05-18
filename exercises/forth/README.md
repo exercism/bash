01f0c4967a198be4fd9aceb680be0461633c76b0
Glenn Jackman <jackman@pythian.com>
2019-05-18 12:01:08 -0400

    port: forth

---
+++ exercises/forth/README.md
@@ -0,0 +1,49 @@
+# Forth
+
+Implement an evaluator for a very simple subset of Forth.
+
+[Forth](https://en.wikipedia.org/wiki/Forth_%28programming_language%29)
+is a stack-based programming language. Implement a very basic evaluator
+for a small subset of Forth.
+
+Your evaluator has to support the following words:
+
+- `+`, `-`, `*`, `/` (integer arithmetic)
+- `DUP`, `DROP`, `SWAP`, `OVER` (stack manipulation)
+
+Your evaluator also has to support defining new words using the
+customary syntax: `: word-name definition ;`.
+
+To keep things simple the only data type you need to support is signed
+integers of at least 16 bits size.
+
+You should use the following rules for the syntax: a number is a
+sequence of one or more (ASCII) digits, a word is a sequence of one or
+more letters, digits, symbols or punctuation that is not a number.
+(Forth probably uses slightly different rules, but this is close
+enough.)
+
+Words are case-insensitive.
+
+
+Run the tests with:
+
+```bash
+bats forth_test.sh
+```
+
+After the first test(s) pass, continue by commenting out or removing the `skip` annotations prepending other tests.
+
+
+
+## External utilities
+`Bash` is a language to write scripts that works closely with various system utilities,
+like [`sed`](https://www.gnu.org/software/sed/), [`awk`](https://www.gnu.org/software/gawk/), [`date`](https://www.gnu.org/software/coreutils/manual/html_node/date-invocation.html) and even other programming languages, like [`Python`](https://www.python.org/).
+This track does not restrict the usage of these utilities, and as long as your solution is portable
+between systems and does not require installing third party applications, feel free to use them to solve the exercise.
+
+For an extra challenge, if you would like to have a better understanding of the language,
+try to re-implement the solution in pure `Bash`, without using any external tools.
+
+## Submitting Incomplete Solutions
+It's possible to submit an incomplete solution so you can see how others have completed the exercise.
---
+++ exercises/forth/example.sh
@@ -0,0 +1,144 @@
+#!/usr/bin/env bash
+
+set -f                  # disable path exansion
+shopt -s extglob        # enable extended patterns
+
+# I'm going to use the stack and the macros as 
+# global variables: Not best practice.
+declare -a stack        # indexed array
+declare -A macros       # associative array
+
+# Encapsulate some stack operations
+stack::len()  { echo ${#stack[@]}; }
+stack::tail() { echo ${stack[-1]}; }
+stack::pop()  { stack=( ${stack[@]:0:$(stack::len)-1} ); }
+stack::push() { stack+=( $* ); }
+
+# True if the first argument consists of digits only
+is_number() { [[ $1 == +([0-9]) ]]; }
+
+die() { echo "$*" >&2; exit 1; }
+
+#
+# The main function
+#
+main() {
+    # Read from stdin
+    while read -r line; do
+
+        # Lower-case the line, and allow word-splitting
+        # Store the words in the positional parameters
+        set -- ${line,,}
+
+        # Keep looping until all the positional params
+        # have been consumed.
+        while (( $# > 0 )); do
+            word=$1
+            shift
+
+            if is_number $word; then
+                stack::push $word
+            elif [[ -n ${macros[$word]} ]]; then
+                set -- ${macros[$word]} $*
+            else
+                case $word in
+                :)
+                    record_macro $*
+                    # Discard rest of line
+                    set --
+                    ;;
+                [-+*/])
+                    # Arithmetic operation
+                    binary_op $word
+                    ;;
+                dup|drop|swap|over)
+                    # Invoke this word as a command
+                    $word
+                    ;;
+                *)
+                    echo "undefined operation" >&2
+                    exit 1
+                    ;;
+                esac
+            fi
+        done
+    done
+
+    echo ${stack[*]}
+}
+
+record_macro() {
+    local macro_name=$1
+    if is_number $macro_name; then 
+        die "illegal operation: cannot redefine number"
+    fi
+    shift
+
+    # Check the last word
+    [[ ${!#} != ";" ]] && die "macro not terminated with semicolon"
+    # pop the semicolon
+    set -- ${@:1:$#-1}
+
+    [[ $# -eq 0 ]] && die "empty macro definition"
+
+    # Check any words in definition for macros
+    local definition=()
+    for word do
+        if [[ -n ${macros[$word]} ]]; then
+            definition+=( ${macros[$word]} )
+        else
+            definition+=( $word )
+        fi
+    done
+    macros[$macro_name]=${definition[*]}
+}
+
+# Check the size of the stack for the number of needed elements
+need() {
+    local -i n=$1
+    local -i len=$(stack::len)
+    (( n > 0 && len == 0 )) && die "empty stack"
+    (( n > 1 && len == 1 )) && die "only one value on the stack"
+}
+
+binary_op() {
+    local op=$1
+
+    need 2
+    local b=$(stack::tail)
+    stack::pop
+    local a=$(stack::tail)
+    stack::pop
+    
+    [[ $op == "/" && $b -eq 0 ]] && die "divide by zero"
+    stack::push $(( a $op b ))
+}
+
+dup() {
+    need 1
+    stack::push $(stack::tail)
+}
+
+drop() {
+    need 1
+    stack::pop
+}
+
+swap() {
+    need 2
+    local b=$(stack::tail)
+    stack::pop
+    local a=$(stack::tail)
+    stack::pop
+    stack::push $b $a
+}
+
+over() {
+    need 2
+    local b=$(stack::tail)
+    stack::pop
+    local a=$(stack::tail)
+    stack::push $b $a
+}
+
+main
