c96ef76039f397e5098dbb55c67c0b5a5d46f511
glennj <jackman@pythian.com>
2019-06-25 08:55:46 -0400

    port: simple-cipher (#327)
    * port: simple-cipher
    
    * insignificant change to trigger new build
    
    * rebase master, explicitly unset nocasematch in exercise
    
    * trying [[:lower:]] instead of [a-z]

---
+++ exercises/simple-cipher/example.sh
@@ -0,0 +1,95 @@
+#!/usr/bin/env bash 
+
+shopt -u nocasematch
+
+main() {
+    local key OPTIND OPTARG
+
+    while getopts :k: opt; do
+        case $opt in
+            k) key=$OPTARG ;;
+            ?) echo "unknown option: -$OPTARG" >&2 ;;
+        esac
+    done
+    shift $((OPTIND - 1))
+
+    [[ -z $key ]] && key=$(generate_key)
+    validate_key "$key"
+
+    case $1 in
+        encode|decode) "$1" "$2" "$key" ;;
+        key) echo "$key" ;;
+        *)   echo "unknown subcommand: $OPTARG" >&2 ;;
+    esac
+}
+
+# ord: the ascii value of a character
+# $ ord "A" #=> 65
+#
+# Note the leading single quote in the last argument 
+# https://www.gnu.org/software/bash/manual/bash.html#index-printf
+#     Arguments to non-string format specifiers (ed: such as %d)
+#     are treated as C language constants, except [...] if
+#     the leading character is a single or double quote, the
+#     value is the ASCII value of the following character.
+
+ord() {
+    printf "%d" "'$1"
+}
+
+# chr: the character represented by the given ASCII decimal value
+# $ chr 65 #=> A
+#
+# Would probably be more performant to use a fixed array of
+# letters and index into it, but this is pretty cool.
+
+chr() {
+    printf "\x$(printf "%x" "$1")"
+}
+
+# 100 random lower case letters
+generate_key() {
+    local key=""
+    local -i a=$(ord "a") i idx
+    for i in {1..100}; do
+        idx=$(( RANDOM % 26 ))
+        key+=$(chr $((a + idx)))
+    done
+    echo "$key"
+}
+
+# the key must contain only lowercase letters
+validate_key() {
+    local key=$1
+    if [[ $key == *[^[:lower:]]* ]]; then
+        echo "invalid key: contains non-lc-letter" >&2
+        exit 1
+    fi
+}
+
+encode() { _code "$1" "$2"  1; }
+
+decode() { _code "$1" "$2" -1; }
+
+_code() {
+    local -l plaintext=$1 key=$2        # lower case
+    local -i direction=$3
+    local -i a=$(ord "a") i idx p k
+    local encoded=""
+
+    # ensure the key is long enough
+    while [[ ${#key} -lt ${#plaintext} ]]; do key+=$key; done
+
+    for (( i = 0; i < ${#plaintext}; i++ )); do
+        p=$(( $(ord "${plaintext:i:1}") - a ))
+        k=$(( $(ord "${key:i:1}")       - a ))
+
+        # (-5 % 26) == -5, but we want to normalize it
+        # to be between 0 and 25
+        idx=$(( (((p + k * direction) % 26) + 26) % 26 ))
+        encoded+=$(chr $((a + idx)))
+    done
+    echo "$encoded"
+}
+
+main "$@"
---
+++ exercises/simple-cipher/simple_cipher_test.sh
@@ -0,0 +1,166 @@
+#!/usr/bin/env bash
+
+# Random key cipher"
+
+@test  "Can generate a random key" {
+    #[[ $BATS_RUN_SKIPPED = true ]] || skip
+    run bash simple_cipher.sh key
+    [[ $status -eq 0 ]]
+    key=$output
+    [[ ${#key} -ge 100 ]]     # at least 100 chars
+    [[ $key != *[^a-z]* ]]    # only letters allowed
+}
+
+@test  "Can encode random" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    run bash simple_cipher.sh key
+    [[ $status -eq 0 ]]
+    key=$output
+    plaintext="aaaaaaaaaa"
+    run bash simple_cipher.sh -k "$key" encode "$plaintext"
+    [[ $status -eq 0 ]]
+    [[ ${#output} -eq 10 ]]
+    [[ $output == "${key:0:10}" ]]
+}
+
+@test  "Can decode random" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    run bash simple_cipher.sh key
+    [[ $status -eq 0 ]]
+    key=$output
+    plaintext="aaaaaaaaaa"
+    run bash simple_cipher.sh -k "$key" decode "${key:0:10}"
+    [[ $status -eq 0 ]]
+    [[ ${#output} -eq 10 ]]
+    [[ $output == "$plaintext" ]]
+}
+
+@test "Is reversible random" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    run bash simple_cipher.sh key
+    [[ $status -eq 0 ]]
+    key=$output
+
+    plaintext="abcdefghij"
+    run bash simple_cipher.sh -k "$key" encode "$plaintext"
+    [[ $status -eq 0 ]]
+    encoded=$output
+
+    run bash simple_cipher.sh -k "$key" decode "$encoded"
+    [[ $status -eq 0 ]]
+    [[ $output == "$plaintext" ]]
+}
+
+# Substitution cipher
+
+@test  "Can encode" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    key=abcdefghij
+    txt=aaaaaaaaaa
+    run bash simple_cipher.sh -k "$key" encode "$txt"
+    [[ $status -eq 0 ]]
+    [[ $output == "$key" ]]
+}
+
+@test  "Can decode" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    key=abcdefghij
+    txt=abcdefghij
+    exp=aaaaaaaaaa
+    run bash simple_cipher.sh -k "$key" decode "$txt"
+    [[ $status -eq 0 ]]
+    [[ $output == "$exp" ]]
+}
+
+@test  "Is reversible" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    key=abcdefghij
+    txt=abcdefghij
+    exp=abcdefghij
+    run bash simple_cipher.sh -k "$key" encode "$txt"
+    [[ $status -eq 0 ]]
+    encoded=$output
+    run bash simple_cipher.sh -k "$key" decode "$encoded"
+    [[ $status -eq 0 ]]
+    [[ $output == "$exp" ]]
+}
+
+@test  "Can double shift encode" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    key=iamapandabear
+    txt=iamapandabear
+    exp=qayaeaagaciai
+    run bash simple_cipher.sh -k "$key" encode "$txt"
+    [[ $status -eq 0 ]]
+    [[ $output == "$exp" ]]
+}
+
+@test  "Can wrap on encode"  {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    key=abcdefghij
+    txt=zzzzzzzzzz
+    exp=zabcdefghi
+    run bash simple_cipher.sh -k "$key" encode "$txt"
+    [[ $status -eq 0 ]]
+    [[ $output == "$exp" ]]
+}
+
+@test  "Can wrap on decode" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    key=abcdefghij
+    txt=zabcdefghi
+    exp=zzzzzzzzzz
+    run bash simple_cipher.sh -k "$key" decode "$txt"
+    [[ $status -eq 0 ]]
+    [[ $output == "$exp" ]]
+}
+
+@test  "Can encode messages longer than the key" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    key=abc
+    txt=iamapandabear
+    exp=iboaqcnecbfcr
+    run bash simple_cipher.sh -k "$key" encode "$txt"
+    [[ $status -eq 0 ]]
+    [[ $output == "$exp" ]]
+}
+
+@test  "Can decode messages longer than the key" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    key=abc
+    txt=iboaqcnecbfcr
+    exp=iamapandabear
+    run bash simple_cipher.sh -k "$key" decode "$txt"
+    [[ $status -eq 0 ]]
+    [[ $output == "$exp" ]]
+}
+
+@test "plaintext is lowercased" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    run bash simple_cipher.sh -k b encode FOOBAR
+    [[ $status -eq 0 ]]
+    [[ $output == "gppcbs" ]]
+}
+
+@test "ciphertext is lowercased" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    run bash simple_cipher.sh -k b decode GPPCBS
+    [[ $status -eq 0 ]]
+    [[ $output == "foobar" ]]
+}
+
+# errors
+
+@test "key must be lowercase" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    run bash simple_cipher.sh -k ABC encode foo
+    [[ $status -eq 1 ]]
+    [[ $output == *"invalid key"* ]]
+}
+
+@test "key must be letters" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    run bash simple_cipher.sh -k 123 encode foo
+    [[ $status -eq 1 ]]
+    [[ $output == *"invalid key"* ]]
+}
