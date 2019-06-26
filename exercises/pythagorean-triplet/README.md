5ef85e990a2813263b45ab43a6af60b9b3e2fc95
glennj <jackman@pythian.com>
2019-06-26 11:49:32 -0400

    port: pythagorean-triplet (#334)
    * port: pythagorean-triplet
    
    * configlet fmt

---
+++ exercises/pythagorean-triplet/README.md
@@ -0,0 +1,70 @@
+# Pythagorean Triplet
+
+A Pythagorean triplet is a set of three natural numbers, {a, b, c}, for
+which,
+
+```text
+a**2 + b**2 = c**2
+```
+
+and such that,
+
+```text
+a < b < c
+```
+
+For example,
+
+```text
+3**2 + 4**2 = 9 + 16 = 25 = 5**2.
+```
+
+Given an input integer N, find all Pythagorean triplets for which `a + b + c = N`.
+
+For example, with N = 1000, there is exactly one Pythagorean triplet for which `a + b + c = 1000`: `{200, 375, 425}`.
+
+
+Run the tests with:
+
+```bash
+bats pythagorean_triplet_test.sh
+```
+
+After the first test(s) pass, continue by commenting out or removing the
+`[[ $BATS_RUN_SKIPPED == true ]] || skip` 
+annotations prepending other tests.
+
+To run all tests, including the ones with `skip` annotations, run:
+
+```bash
+BATS_RUN_SKIPPED=true bats pythagorean_triplet_test.sh
+```
+
+## Source
+
+Problem 9 at Project Euler [http://projecteuler.net/problem=9](http://projecteuler.net/problem=9)
+
+
+## External utilities
+`Bash` is a language to write "scripts" -- programs that can call
+external tools, such as
+[`sed`](https://www.gnu.org/software/sed/),
+[`awk`](https://www.gnu.org/software/gawk/),
+[`date`](https://www.gnu.org/software/coreutils/manual/html_node/date-invocation.html)
+and even programs written in other programming languages, 
+like [`Python`](https://www.python.org/).
+This track does not restrict the usage of these utilities, and as long
+as your solution is portable between systems and does not require
+installation of third party applications, feel free to use them to solve
+the exercise.
+
+For an extra challenge, if you would like to have a better understanding
+of the language, try to re-implement the solution in pure `Bash`,
+without using any external tools. Note that there are some types of
+problems that bash cannot solve, such as performing floating point
+arithmetic and manipulating dates: for those, you must call out to an
+external tool.
+
+## Submitting Incomplete Solutions
+It's possible to submit an incomplete solution so you can see how others
+have completed the exercise.
---
+++ exercises/pythagorean-triplet/pythagorean_triplet_test.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+# Output your triplets, comma-separated, one per line.
+
+
+@test "triplets whose sum is 12" {
+    #[[ $BATS_RUN_SKIPPED = true ]] || skip
+    run bash pythagorean_triplet.sh 12
+    [[ $status -eq 0 ]]
+    actual=$( sort -t, -n -k1,1 <<< "$output" )
+    expected="3,4,5"
+    [[ $actual == "$expected" ]]
+}
+
+@test "triplets whose sum is 108" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    run bash pythagorean_triplet.sh 108
+    [[ $status -eq 0 ]]
+    actual=$( sort -t, -n -k1,1 <<< "$output" )
+    expected="27,36,45"
+    [[ $actual == "$expected" ]]
+}
+
+@test "triplets whose sum is 1000" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    run bash pythagorean_triplet.sh 1000
+    [[ $status -eq 0 ]]
+    actual=$( sort -t, -n -k1,1 <<< "$output" )
+    expected="200,375,425"
+    [[ $actual == "$expected" ]]
+}
+
+@test "no matching triplets for 1001" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    run bash pythagorean_triplet.sh 1001
+    [[ $status -eq 0 ]]
+    [[ $output == "" ]]
+}
+
+@test "returns all matching triplets" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    run bash pythagorean_triplet.sh 90
+    [[ $status -eq 0 ]]
+    actual=$( sort -t, -n -k1,1 <<< "$output" )
+    expected=$'9,40,41\n15,36,39'
+    [[ $actual == "$expected" ]]
+}
+
+@test "several matching triplets" {
+    [[ $BATS_RUN_SKIPPED = true ]] || skip
+    run bash pythagorean_triplet.sh 840
+    [[ $status -eq 0 ]]
+    actual=$( sort -t, -n -k1,1 <<< "$output" )
+    expected="40,399,401
+56,390,394
+105,360,375
+120,350,370
+140,336,364
+168,315,357
+210,280,350
+240,252,348"
+    [[ $actual == "$expected" ]]
+}
+
+# This test is very time-consuming for a brute force solution.
+# Uncomment and run only for extra credit (or punishment).
+
+#@test "triplets for large number" {
+#    [[ $BATS_RUN_SKIPPED = true ]] || skip
+#    run bash pythagorean_triplet.sh 30000
+#    [[ $status -eq 0 ]]
+#    actual=$( sort -t, -n -k1,1 <<< "$output" )
+#    expected="1200,14375,14425
+#1875,14000,14125
+#5000,12000,13000
+#6000,11250,12750
+#7500,10000,12500"
+#    [[ $actual == "$expected" ]]
+#}
