df6b07a0051e152ba0e00fdc2a7804235b198ef5
Glenn Jackman <jackman@pythian.com>
2019-07-21 18:56:55 -0400

    We should be doing string matching for output tests, not numeric

--- exercises/knapsack/knapsack_test.sh
+++ exercises/knapsack/knapsack_test.sh
@@ -18,47 +18,47 @@
     [[ $BATS_RUN_SKIPPED = true ]] || skip
     run bash knapsack.sh 100
     [[ $status -eq 0 ]]
-    [[ $output -eq 0 ]]
+    [[ $output == "0" ]]
 }
 
 @test "one item: too heavy" {
     [[ $BATS_RUN_SKIPPED = true ]] || skip
     run bash knapsack.sh 10 100:1
     [[ $status -eq 0 ]]
-    [[ $output -eq 0 ]]
+    [[ $output == "0" ]]
 }
 
 @test "five items (cannot be greedy by weight)" {
     [[ $BATS_RUN_SKIPPED = true ]] || skip
     run bash knapsack.sh 10 2:5 2:5 2:5 2:5 10:21
     [[ $status -eq 0 ]]
-    [[ $output -eq 21 ]]
+    [[ $output == "21" ]]
 }
 
 @test "five items (cannot be greedy by value)" {
     [[ $BATS_RUN_SKIPPED = true ]] || skip
     run bash knapsack.sh 10 2:20 2:20 2:20 2:20 10:50
     [[ $status -eq 0 ]]
-    [[ $output -eq 80 ]]
+    [[ $output == "80" ]]
 }
 
 @test "example knapsack" {
     [[ $BATS_RUN_SKIPPED = true ]] || skip
     run bash knapsack.sh 10 5:10 4:40 6:30 4:50
     [[ $status -eq 0 ]]
-    [[ $output -eq 90 ]]
+    [[ $output == "90" ]]
 }
 
 @test "8 items" {
     [[ $BATS_RUN_SKIPPED = true ]] || skip
     run bash knapsack.sh 104 25:350 35:400 45:450 5:20 25:70 3:8 2:5 2:5
     [[ $status -eq 0 ]]
-    [[ $output -eq 900 ]]
+    [[ $output == "900" ]]
 }
 
 @test "15 items" {
     [[ $BATS_RUN_SKIPPED = true ]] || skip
     run bash knapsack.sh 750 70:135 73:139 77:149 80:150 82:156 87:163 90:173 94:184 98:192 106:201 110:210 113:214 115:221 118:229 120:240
     [[ $status -eq 0 ]]
-    [[ $output -eq 1458 ]]
+    [[ $output == "1458" ]]
 }
