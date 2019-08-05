acc1f86b830ce5def0f72edef53dba248acfb682
guy <strings.stringandstrings@gmail.com>
2019-08-05 19:58:49 -0400

    triangle: lib tests without namespacing

--- exercises/triangle/triangle.sh
+++ exercises/triangle/triangle.sh
@@ -1,24 +1,13 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+equilateral() {
+  echo 'unimplemented!'
+}
+
+isosceles() {
+  echo 'unimplemented!'
+}
+
+scalene() {
+  echo 'unimplemented!'
+}
