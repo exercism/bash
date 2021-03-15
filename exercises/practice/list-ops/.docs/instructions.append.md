88621cd4dceb4939dddfc91946b8b1c3be5de7c3
Glenn Jackman <glenn.jackman@gmail.com>
2021-03-15 10:49:34 -0400

    list-ops: create exercise (#376)
    * WIP: list-ops: create exercise
    
    * list-ops: rework for v3 directory structure
    - add dot files
    - add blurb about "library exercise"
    
    * list-ops: uses namerefs that require bash 4.3
    
    * Fix GHA PR script: accurately identify files that need testing

---
+++ exercises/practice/list-ops/.docs/instructions.append.md
@@ -0,0 +1,28 @@
+# Library of Functions
+
+This is the first exercise we've seen where the solution we're writing
+is not a "main" script. We're writing a library to be "source"d into
+other scripts that will invoke our functions.
+
+## Bash namerefs
+
+This exercise requires the use of `nameref` variables. This requires a bash
+version of at least 4.0. If you're using the default bash on MacOS, you'll
+need to install another version: see [Installing Bash](https://exercism.io/tracks/bash/installation)
+
+Namerefs are a way to pass a variable to a function _by reference_. That
+way, the variable can be modified in the function and the updated value is
+available in the calling scope. Here's an example:
+```bash
+prependElements() {
+    local -n __array=$1
+    shift
+    __array=( "$@" "${__array[@]}" )
+}
+
+my_array=( a b c )
+echo "before: ${my_array[*]}"    # => before: a b c
+
+prependElements my_array d e f
+echo "after: ${my_array[*]}"     # => after: d e f a b c
+```
---
+++ exercises/practice/list-ops/list_ops.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+if [[ "${BASH_SOURCE[0]}" == "$0" ]]; then
+    echo "This library of functions should be sourced into another script" >&2
+    exit 4
+fi
+bash_version=$((10 * BASH_VERSINFO[0] + BASH_VERSINFO[1]))
+if (( bash_version < 43 )); then
+    echo "This library requires at least bash version 4.3" >&2
+    return 4
+fi
+
+# Due to inherent bash limitations around word splitting and globbing,
+# functions that are intended to *return a list* are instead required to
+# receive a nameref parameter, the name of an array variable that will be
+# populated in the list function.
+# See the filter, map and reverse functions.
+
+# Also note that nameref parameters cannot have the same name as the
+# name of the variable in the calling scope.
+
+
+# Append some elements to the given list.
+list::append () {
+    echo "Implement me" >&2
+    return 1
+}
+
+# Return only the list elements that pass the given function.
+list::filter () {
+    echo "Implement me" >&2
+    return 1
+}
+
+# Transform the list elements, using the given function,
+# into a new list.
+list::map () {
+    echo "Implement me" >&2
+    return 1
+}
+
+# Left-fold the list using the function and the initial value.
+list::foldl () {
+    echo "Implement me" >&2
+    return 1
+}
+
+# Right-fold the list using the function and the initial value.
+list::foldr () {
+    echo "Implement me" >&2
+    return 1
+}
+
+# Return the list reversed
+list::reverse () {
+    echo "Implement me" >&2
+    return 1
+}
