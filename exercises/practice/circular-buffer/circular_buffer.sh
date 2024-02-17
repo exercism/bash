c9749aa653210df7c797613096d02dabb6b284f0
Glenn Jackman <jackman@pythian.com>
2024-02-16 21:15:37 -0500

    An empty stub

--- exercises/practice/circular-buffer/circular_buffer.sh
+++ exercises/practice/circular-buffer/circular_buffer.sh
@@ -7,88 +7,49 @@ if (( bash_version < 43 )); then
     return 4
 fi
 
-# I'm storing all the data for all circular buffers in a single global
-# associative array.
-# Empty cells are denoted by empty strings.
-# Therefore, an empty string cannot be stored in this circular buffer
-
-declare -gA _buffer_data=()
-
+# Create a new circular buffer with the given name and size.
 buffer::new() {
-    local name=$1
-    local size=$2
-    _buffer_data[size,$name]=$size
-    buffer::clear "$name"
+    local name=$1 size=$2
+    echo "Implement me" >&2
+    exit 1
 }
 
+# Optionally, destroy an existing circular buffer.
+# Your implementation may not need to do anything; if so leave this function empty.
 buffer::destroy() {
     local name=$1
-    for ((idx = 0; idx < ${_buffer_data[size,$name]}; idx++)); do
-        unset "_buffer_data[$idx,$name]"
-    done
-    unset "_buffer_data[read,$name]"
-    unset "_buffer_data[write,$name]"
-    unset "_buffer_data[size,$name]"
 }
 
+# Wipe out any existing data in the buffer.
 buffer::clear() {
     local name=$1
-    _buffer_data[read,$name]=0
-    _buffer_data[write,$name]=0
-    for ((idx = 0; idx < ${_buffer_data[size,$name]}; idx++)); do
-        _buffer_data[$idx,$name]=''
-    done
+    echo "Implement me" >&2
+    exit 1
 }
 
-buffer::is_empty() {
-    local name=$1
-    local idx=${_buffer_data[read,$name]}
-    [[ -z ${_buffer_data[$idx,$name]} ]]
-}
-
-buffer::is_full() {
-    local name=$1
-    local idx=${_buffer_data[write,$name]}
-    [[ -n ${_buffer_data[$idx,$name]} ]]
-}
-
+# Find the oldest element in the buffer.
+# Set the given variable to hold the value.
+# Remove the element from the buffer.
+# Return a failure status if the buffer is empty, otherwise return success.
 buffer::read() {
     local name=$1
     local -n _read_value=$2
-    if buffer::is_empty "$name"; then
-        return 1
-    fi
-
-    local idx=${_buffer_data[read,$name]}
-    _read_value=${_buffer_data[$idx,$name]}
-    _buffer_data[$idx,$name]=''
-
-    buffer::incr_pointer "$name" read
+    echo "Implement me" >&2
+    exit 1
 }
 
+# Write a new value into the buffer.
+# Return a failure status if the buffer is full, otherwise return success.
 buffer::write() {
     local name=$1 item=$2
-    if buffer::is_full "$name"; then
-        return 1
-    fi
-
-    local idx=${_buffer_data[write,$name]}
-    _buffer_data[$idx,$name]=$item
-
-    buffer::incr_pointer "$name" write
+    echo "Implement me" >&2
+    exit 1
 }
 
-buffer::incr_pointer() {
-    local name=$1 p=$2
-    _buffer_data[$p,$name]=$(( (_buffer_data[$p,$name] + 1) % _buffer_data[size,$name] ))
-}
-
+# Write a new value into the buffer.
+# If the buffer is full, overwrite the oldest element.
 buffer::overwrite() {
     local name=$1 item=$2
-    if buffer::is_full "$name"; then
-        # shellcheck disable=SC2034
-        local tmp
-        buffer::read "$name" tmp
-    fi
-    buffer::write "$name" "$item"
+    echo "Implement me" >&2
+    exit 1
 }
