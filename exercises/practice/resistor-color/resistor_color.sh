f84c1aa9a25d9c06d67e1d404269cc4a94b92fd3
Glenn Jackman <jackman@pythian.com>
2025-02-10 10:30:14 -0500

    stub file

--- exercises/practice/resistor-color/resistor_color.sh
+++ exercises/practice/resistor-color/resistor_color.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
-readonly Colors=( black brown red orange yellow green blue violet grey white )
-
-main () {
-    case "$1" in
-        colors)
-            printf '%s\n' "${Colors[@]}"
-            ;;
-
-        code)
-            for idx in "${!Colors[@]}"; do
-                if [[ "${Colors[idx]}" == "$2" ]]; then
-                    echo "$idx"
-                    break
-                fi
-            done
-            ;;
-    esac
-}
-
-main "$@"
+# The following comments should help you get started:
+# - Bash is flexible. You may use functions or write a "raw" script.
+#
+# - Complex code can be made easier to read by breaking it up
+#   into functions, however this is sometimes overkill in bash.
+#
+# - You can find links about good style and other resources
+#   for Bash in './README.md'. It came with this exercise.
+#
+#   Example:
+#   # other functions here
+#   # ...
+#   # ...
+#
+#   main () {
+#     # your main function code here
+#   }
+#
+#   # call main with all of the positional arguments
+#   main "$@"
+#
+# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
