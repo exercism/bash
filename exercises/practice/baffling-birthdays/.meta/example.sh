bd1d6d0254ae93b4be19f28d7f4f41d23f078060
resu-xuniL <linux@lavache.com>
2026-06-13 00:32:44 +0200

    Update `example.sh` and `baffling_birthdays.bats`
    * Co-authored-by: IsaacG
    * Co-authored-by: glennj

--- exercises/practice/baffling-birthdays/.meta/example.sh
+++ exercises/practice/baffling-birthdays/.meta/example.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
+current_year=$(date -d "now" +%Y)
+
 main() {
     local function=$1; shift
 
-    $function "$@"
+    "$function" "$@"
 }
 
-sharedBirthday () {
+shared_birthday () {
     local -a birthdates=( "$@" )
     local -A birthdays
 
     for birthdate in "${birthdates[@]}"; do
         birthday=${birthdate: -5}
 
-        (( birthdays[$birthday] ++ ))
+        (( birthdays[$birthday]++ ))
     done
 
     if (( ${#birthdays[@]} == ${#birthdates[@]} )); then
@@ -23,54 +25,51 @@ sharedBirthday () {
     fi
 }
 
-randomBirthdates () {
+random_int() {
+    echo $(( RANDOM % $1 ))
+}
+
+random_birthdates () {
     local -i number=$1
-    local -i actual_year random_year random_month random_day
+    local -i random_year random_month random_day
     local -a birthdates
 
-    actual_year=$(date -d "now" +%Y)
-
-    for ((i = 0; i < number; i++)); do
-        random_year=$(shuf -n 1 -i 1900-${actual_year})
-        random_month=$(shuf -n 1 -i 1-12)
+    for (( i = 0; i < number; i++ )); do
+        random_year=$(( 1900 + $(random_int $(( current_year - 1900 + 1 ))) ))
+        random_month=$(( 1 + $(random_int 12) ))
 
         if (( random_year % 400 == 0 || (random_year % 4 == 0 && random_year % 100 != 0) )); then
             (( random_year ++ ))
         fi
 
-        if (( random_month == 2 )); then
-            random_day=$(shuf -n 1 -i 1-28)
-        elif [[ $random_month =~ ^(4|6|9|11)$ ]]; then
-            random_day=$(shuf -n 1 -i 1-30)
-        else
-            random_day=$(shuf -n 1 -i 1-31)
-        fi
+        case "$random_month" in
+                   2) random_day=$(( 1 + $(random_int 28) )) ;;
+            4|6|9|11) random_day=$(( 1 + $(random_int 30) )) ;;
+                   *) random_day=$(( 1 + $(random_int 31) )) ;;
+        esac
 
-        birthdates+=( "$(date -d "${random_year}-${random_month}-${random_day}" +%F)" )
+        birthdates+=( "$(printf '%d-%02d-%02d' "$random_year" "$random_month" "$random_day")" )
     done
 
     echo "${birthdates[@]}"
 }
 
-estimatedProbabilityOfSharedBirthday () {
+estimated_probability_of_share_birthday () {
     local -i group_size=$1
-    # the more you increase the 'runs' value, 
+    # the more you increase the 'runs' value,
     # the more the result is approching the expected one
     # but the more you increase the computing time!
-    local -i runs=1000
+    local -i runs=2000
     local -i count=0
-    local result
 
-
-    for ((i = 0; i <= runs; i++)); do
-        if [[ $(sharedBirthday $(randomBirthdates $group_size)) == "true" ]]; then
-            (( ++ count ))
+    for (( i = 0; i <= runs; i++ )); do
+        read -ra birthdates < <(random_birthdates "$group_size")
+        if [[ "$(shared_birthday "${birthdates[@]}")" == "true" ]]; then
+            (( count++ ))
         fi
     done
 
-    result="$(LC_NUMERIC=C printf "%.2f\n" "$(echo "scale=2; ($count * 100) / $runs" | bc)")"
-
-    echo "$result"
+    echo $(( count * 100 / runs ))
 }
 
 main "$@"
